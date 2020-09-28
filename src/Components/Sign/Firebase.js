5cb329fda0824b76b37bca4739e52b4dcab29586
my name <you@exampl.com>
2020-09-29 01:07:37 +0600

    goole signIn system added

--- src/Components/Sign/Firebase.js
+++ src/Components/Sign/Firebase.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Sing.css'
+import * as firebase from "firebase/app"
+import "firebase/auth"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faGoogle, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import firebaseConfig from './FirebaseConfig';
+import { GlobalData } from '../Main/Main';
 
+firebase.initializeApp(firebaseConfig);
 const Firebase = () => {
+    const setLoginInfo = useContext(GlobalData).login[1]
+    const collectLoginInfo = (displayName, email, photoURL) => {
+        setLoginInfo({
+            displayName: displayName,
+            email: email,
+            photoURL: photoURL
+        })
+    }
+
+    /*---------start---------google signIn area---------start---------*/
+    const googleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            const { displayName, email, photoURL } = result.user
+            collectLoginInfo(displayName, email, photoURL)
+        }).catch(function (error) {
+            const { code, message, email, credential } = error;
+            console.log(code, "| |", message, "| |", email, "| |", credential);
+        });
+    }
+    /*---------end---------google signIn area---------end---------*/
+
+    /*---------start---------facebook signIn area---------start---------*/
+    const facebookSignIn = () => {
+
+    }
+    /*---------end---------facebook signIn area---------end---------*/
+
+    /*---------start---------twitter signIn area---------start---------*/
+    const twitterSignIn = () => {
+
+    }
+    /*---------end---------twitter signIn area---------end---------*/
     return (
         <section className="firebase-auth mt-4">
             <h4 className="mx-auto">Or continue with</h4>
             <div className="brand-icons d-flex justify-content-center">
-                <span className="google-icon"><FontAwesomeIcon icon={faGoogle} /></span>
-                <span className="facebook-icon"><FontAwesomeIcon icon={faFacebook} /></span>
-                <span className="twitter-icon"><FontAwesomeIcon icon={faTwitter} /></span>
+                <span className="google-icon"><FontAwesomeIcon icon={faGoogle} onClick={googleSignIn} /></span>
+                <span className="facebook-icon"><FontAwesomeIcon icon={faFacebook} onClick={facebookSignIn} /></span>
+                <span className="twitter-icon"><FontAwesomeIcon icon={faTwitter} onClick={twitterSignIn} /></span>
             </div>
         </section>
     );
---
+++ src/Components/Sign/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbxBO1WrpXscV2rkIdBMjtkd58cPxFzig",
+    authDomain: "the-red-onion-foods.firebaseapp.com",
+    databaseURL: "https://the-red-onion-foods.firebaseio.com",
+    projectId: "the-red-onion-foods",
+    storageBucket: "the-red-onion-foods.appspot.com",
+    messagingSenderId: "695910864848",
+    appId: "1:695910864848:web:2b1a456dd8d6fd1e758bd5"
+};
+
+export default firebaseConfig;
